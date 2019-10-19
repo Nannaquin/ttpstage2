060da655f1addb63c19c277fb105bf7afa35ca80
Steven Santana <stvnmsantana@gmail.com>
2019-10-19 10:48:38 -0400

    Enable deployment to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 
@@ -32,6 +33,16 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+// Serve Static Assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000
 // process.env.pot is Heroku's port if one chooses to deploy there
